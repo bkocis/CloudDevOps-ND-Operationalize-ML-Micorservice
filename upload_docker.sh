c6e8d931a3e2010b0f9838c32f5ed85cd2f9730f
bkocis <balaz.kocis@gmail.com>
2020-08-27 11:21:18 +0000

    docker upload script corrected

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,18 +5,25 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=bkocis/mlapp
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login --username bkocis
+docker tag mlapp bkocis/mlapp
+
 # Step 3:
 # Push image to a docker repository
 
+docker push $dockerpath
 
+
+
+# Upload to AWS ECR
 # tag image 
-docker tag mlapp:latest 643112058200.dkr.ecr.eu-central-1.amazonaws.com/mlapp:latest
+# docker tag mlapp:latest 643112058200.dkr.ecr.eu-central-1.amazonaws.com/mlapp:latest
 
 # push image to AWS repository
-docker push 643112058200.dkr.ecr.eu-central-1.amazonaws.com/mlapp:latest
+# docker push 643112058200.dkr.ecr.eu-central-1.amazonaws.com/mlapp:latest
